b7485abc2994d148cf993e0bc76b4c44dfcd4959
saman-re <68374661+saman-re@users.noreply.github.com>
2023-03-14 03:03:25 +0330

    improve: add sample api for get request

--- src/main/kotlin/com/example/tapselltask/EventController.kt
+++ src/main/kotlin/com/example/tapselltask/EventController.kt
@@ -1,9 +1,24 @@
 package com.example.tapselltask
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 @RestController
-@RequestMapping(path = ["api/v1"])
-class EventController {
+@RequestMapping(path = ["/api/v1"])
+class EventController(
+    @Autowired
+    private val eventService: EventService
+) {
+
+    @GetMapping
+    fun checkHealth(
+        @RequestParam(name = "publisherId", required = false) advertiserId: Optional<String>,
+        @RequestParam(name = "publisherId", required = false) publisherId: Optional<String>
+    ): String {
+        return "salam $advertiserId $publisherId"
+    }
 }
