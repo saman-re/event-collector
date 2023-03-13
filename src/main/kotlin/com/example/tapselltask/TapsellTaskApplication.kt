8a8882d725d49b3f5632cca525471632bbf46ed7
saman-re <68374661+saman-re@users.noreply.github.com>
2023-03-14 02:40:03 +0330

    improve: enable scheduling in application

--- src/main/kotlin/com/example/tapselltask/TapsellTaskApplication.kt
+++ src/main/kotlin/com/example/tapselltask/TapsellTaskApplication.kt
@@ -1,11 +1,13 @@
 package com.example.tapselltask
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class TapsellTaskApplication
 
 fun main(args: Array<String>) {
-    runApplication<TapsellTaskApplication>(*args)
+    SpringApplication.run(TapsellTaskApplication::class.java,*args)
 }
