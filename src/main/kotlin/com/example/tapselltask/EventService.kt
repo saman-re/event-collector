dd5288570f5f8d1bcf7b2d76975468f02553a239
saman-re <68374661+saman-re@users.noreply.github.com>
2023-03-14 02:55:40 +0330

    improve: add event service to project

---
+++ src/main/kotlin/com/example/tapselltask/EventService.kt
@@ -0,0 +1,21 @@
+package com.example.tapselltask
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+
+@Service
+class EventService {
+
+    val builder = WebClient.builder().codecs { codecs -> codecs.defaultCodecs().maxInMemorySize(1000 * 1024) }
+    val startTime:Long = 1
+
+    @Scheduled(fixedRate = 60000)
+    fun eventFetch() {
+        val timeMillis = System.currentTimeMillis()
+        val since: Long = timeMillis - startTime * 60000
+        val url = "http://192.168.1.57:8071/api/events?since=$since"
+        val response:EventResponse? = builder.build().get().uri(url).retrieve().bodyToMono(EventResponse::class.java).block()
+        println(response!!.events[0].toString())
+    }
+}
