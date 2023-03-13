731ff12414492d58d275d9335772d787fe1d8f9a
saman-re <68374661+saman-re@users.noreply.github.com>
2023-03-14 00:13:49 +0330

    initial commit

---
+++ src/main/kotlin/com/example/tapselltask/TapsellTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tapselltask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TapsellTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<TapsellTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/tapselltask/TapsellTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.tapselltask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TapsellTaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
