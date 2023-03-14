458a2c60bc57175eaeb8d60ea0ff9617670a4fc5
saman-re <68374661+saman-re@users.noreply.github.com>
2023-03-14 05:50:19 +0330

    chore: import autowired annotation

--- src/main/kotlin/com/example/tapselltask/EventService.kt
+++ src/main/kotlin/com/example/tapselltask/EventService.kt
@@ -1,5 +1,6 @@
 package com.example.tapselltask
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
