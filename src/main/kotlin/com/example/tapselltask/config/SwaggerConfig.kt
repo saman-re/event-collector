dd6bf9bb988cf57f5f36cd49c11fd2e0b52559b7
saman-re <68374661+saman-re@users.noreply.github.com>
2023-04-03 00:40:00 +0330

    improve: add meta info to swagger doc

--- src/main/kotlin/com/example/tapselltask/config/SwaggerConfig.kt
+++ src/main/kotlin/com/example/tapselltask/config/SwaggerConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -19,6 +20,21 @@ class SwaggerConfig {
             .apis(RequestHandlerSelectors.basePackage("com.example.tapselltask"))
             .paths(PathSelectors.regex("/api/v1.*"))
             .build()
+            .apiInfo(metaInfo())
 
+    }
+
+    private fun metaInfo(): ApiInfo? {
+
+        return ApiInfo(
+            "Tapsell backer entry task",
+            "This single api is for tapsell backend team entry task documented using swagger",
+            "1.0",
+            "Terms of Service",
+            "saman",
+            "Apache License Version 2.0",
+            "https://www.apache.org/license.html"
+
+        )
     }
 }
