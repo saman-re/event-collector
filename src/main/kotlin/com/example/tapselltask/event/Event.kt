fad6c50c1b036ba25e35f9d367488cf8e348ba3f
saman-re <68374661+saman-re@users.noreply.github.com>
2023-04-02 19:19:49 +0330

    refactor: change Integer to Int

--- src/main/kotlin/com/example/tapselltask/event/Event.kt
+++ src/main/kotlin/com/example/tapselltask/event/Event.kt
@@ -4,12 +4,12 @@ class Event(
     val impressionId:String,
     val eventType:String,
     val eventTime:Long,
-    val advertiserId:Integer?,
-    val publisherId:Integer?,
+    val advertiserId: Int?,
+    val publisherId: Int?,
     val advertiserExpenseType:String?,
     val publisherIncomeType:String?,
-    val advertiserExpense:Integer?,
-    val publisherIncome:Integer?
+    val advertiserExpense: Int?,
+    val publisherIncome: Int?
 ){
     override fun toString(): String {
         return "Event(impressionId='$impressionId', eventType='$eventType', eventTime=$eventTime, advertiserId=$advertiserId, publisherId=$publisherId, advertiserExpenseType='$advertiserExpenseType', publisherIncomeType='$publisherIncomeType', advertiserExpense=$advertiserExpense, publisherIncome=$publisherIncome)"
