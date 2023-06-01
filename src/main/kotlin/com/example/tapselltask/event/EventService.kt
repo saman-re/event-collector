30b42753727317829a86834959233c07c5087838
saman-re <68374661+saman-re@users.noreply.github.com>
2023-06-02 01:44:37 +0330

    improve: update url to request event generator service using docker dns

--- src/main/kotlin/com/example/tapselltask/event/EventService.kt
+++ src/main/kotlin/com/example/tapselltask/event/EventService.kt
@@ -1,14 +1,11 @@
 package com.example.tapselltask.event
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 class EventService(
-    @Autowired
-    val eventRepository: EventRepository
 ) {
 
     val builder = WebClient.builder().codecs { codecs -> codecs.defaultCodecs().maxInMemorySize(1000 * 1024) }
@@ -18,8 +15,9 @@ class EventService(
     fun eventFetch() {
         val timeMillis = System.currentTimeMillis()
         val since: Long = timeMillis - startTime * 60000
-        val url = "http://192.168.1.57:8071/api/events?since=$since"
-        val response:EventResponse? = builder.build().get().uri(url).retrieve().bodyToMono(EventResponse::class.java).block()
+        val url = "http://events:8071/api/events?since=$since"
+        val response: EventResponse? = builder.build().get().uri(url).retrieve().bodyToMono(EventResponse::class.java).block()
+//        eventRepository.saveAll(response!!.events)
         println(response!!.events[0].toString())
     }
 
